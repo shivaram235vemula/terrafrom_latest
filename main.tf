28a6bbc0bc9fb31f290067c700ee7aa6a1613391
babakabulldozer <94636162+shivaram235vemula@users.noreply.github.com>
2025-05-07 14:43:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "shivaram-demo-terraform-s3jdkfnjakn"
+  bucket = "shivaram-demo-terraform-s3jdkfnjdfsdakn"
 
   tags = {
-    Name        = "shivaram-demo-terraform-s3dfjdnjkn"
+    Name        = "shivaram-demo-terraform-s3dfjdnjkdfdsn"
     Environment = "dev"
   }
 }
